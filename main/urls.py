3dfea5dff71207a0e721028b3bdb7fd33eb5a8da
f.i.xodjaev <farruhcool1993@gmail.com>
2021-07-02 14:23:49 +0500

    Added user app and login page

--- main/urls.py
+++ main/urls.py
@@ -18,7 +18,10 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 
+from user import views as user_views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('dashboard.urls')),
+    path('login/', user_views.login, name='user-login'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
